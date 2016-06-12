8ca13e6557509442d78ae14f1474c81063adc18b
mondaychen <monday.chen@gmail.com>
2016-06-12 11:59:02 -0400

    change to text

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import { extend } from 'underscore'
 import Backbone from 'backbone'
-import Two from 'two.js'
 
 import getSVG from './svg'
 
@@ -18,31 +17,29 @@ app.init = function(rootEl) {
   shape.translation.x = 120
   shape.translation.y = 100
   shape.scale = 0.1
-  shape.fill = 'blue'
 
+  var text = two.makeText('MD.ai', 0, 0)
+  text.size = 18
+  text.fill = 'rgba(255, 192, 192, 1)'
 
+  var text = two.makeGroup(text)
+  text.translation.set(two.width - 30 , two.height - 30)
+  text.scale = 0
+  text.opacity = 0
 
-  var circle = two.makeCircle(-70, 0, 50)
-  var rect = two.makeRectangle(70, 0, 100, 100)
-  circle.fill = '#FF8000'
-  rect.fill = 'rgba(0, 200, 255, 0.75)'
-
-  var group = two.makeGroup(circle, rect)
-  group.translation.set(two.width / 2, two.height / 2)
-  group.scale = 0
-  group.noStroke()
-
-  // Bind a function to scale and rotate the group
+  // Bind a function to scale and rotate the text
   // to the animation loop.
   two.on('update', function(frameCount) {
     // This code is called everytime two.update() is called.
     // Effectively 60 times per second.
-    if (group.scale > 0.9999) {
-      group.scale = group.rotation = 0
+    if (text.scale > 0.9999) {
+      text.scale = text.opacity = text.rotation = 0
     }
-    var t = (1 - group.scale) * 0.125
-    group.scale += t
-    group.rotation += t * 4 * Math.PI
+    var t = (1 - text.scale) * 0.125
+    text.scale += t
+    text.rotation += t * 4 * Math.PI
+    t = (1 - text.opacity) * 0.05
+    text.opacity += t
   }).play()  // Finally, start the animation loop
 }
 
