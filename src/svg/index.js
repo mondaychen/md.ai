a6cc89d8edd8a4ad35ec32d02e52f40812595f0e
mondaychen <monday.chen@gmail.com>
2016-06-12 22:59:10 -0400

    add shocked face 1

--- src/svg/index.js
+++ src/svg/index.js
@@ -2,12 +2,14 @@ import happy1 from './faces/happy-1.svg'
 import happy4 from './faces/happy-4.svg'
 import crying from './faces/crying.svg'
 import sad from './faces/sad.svg'
+import shocked1 from './faces/shocked-1.svg'
 
 export const SVG_LIB = {
   happy1,
   happy4,
   crying,
-  sad
+  sad,
+  shocked1
 }
 
 export default function getSVG(name) {
