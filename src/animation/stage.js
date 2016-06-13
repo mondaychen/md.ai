942f6a98d3c4a1dc36139884b1bd89460d8f8e43
mondaychen <monday.chen@gmail.com>
2016-06-12 20:10:22 -0400

    fix naming

--- src/animation/stage.js
+++ src/animation/stage.js
@@ -1,6 +1,6 @@
 import getSVG from 'svg'
 
-export default function logo(two) {
+export default function stage(two) {
   const face = getSVG('happy1')
   var shape = two.interpret(face).center()
   shape.translation.x = 120
